261465531b7d1ff805b6a157702d2b7cc7d15ecf
Salome Rodriguez <rodriguez.salos@gmail.com>
2025-05-24 18:04:10 -0600

    JS_Entrega3 Agregando API - chiste.js

---
+++ js/chiste.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const btnChiste = document.getElementById('btnChiste');
+  const pChiste = document.getElementById('chisteDia');
+
+  if (btnChiste && pChiste) {
+    btnChiste.addEventListener('click', async () => {
+      try {
+        const res = await fetch('https://official-joke-api.appspot.com/random_joke');
+        const data = await res.json();
+        const joke = `${data.setup} — ${data.punchline}`;
+        pChiste.textContent = "😜"+ joke + "🤡";
+        pChiste.style.color = 'yellow';
+
+        // Agrega clases de animación
+        pChiste.classList.remove('animate__fadeInUp'); // Reinicia animación
+        void pChiste.offsetWidth; // Forzar reflow
+        pChiste.classList.add('animate__animated', 'animate__fadeInUp');
+      } catch (error) {
+        pChiste.textContent = "No se pudo cargar el chiste. Intenta más tarde.";
+        console.error("Error al consultar el chiste:", error);
+      }
+    });
+  }
+});
